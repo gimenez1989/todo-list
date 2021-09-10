6da6e86c72b2d622dfa55f9a0fedd0febb3f962b
Carlos Gimenez <gimenez.22axl@gmail.com>
2021-09-10 19:55:48 -0300

    practica de lista

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import TareaForm from './componentes/TareasFrom';
+import Tarea from './componentes/tareas';
 import './App.css';
 
 function App() {
+  const [listaTareas, setListaTareas] = useState([])
+
+
+  const nuevaTarea = (tarea) => {
+    setListaTareas([tarea, ...listaTareas])
+  }
+
+  const borrar = (id) => {
+    const listaFiltrada = listaTareas.filter((e, index) => index !== id)
+    setListaTareas(listaFiltrada)
+  }
+
+  const actualizarTarea = (id, tarea) => {
+    const listaActualizada = listaTareas.map((e, index) => {
+      if(index === id){
+        e = tarea
+      }
+
+      return e
+    })
+    
+    setListaTareas(listaActualizada)
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TareaForm 
+        nuevaTarea={nuevaTarea}
+      />
+
+      <div className="lista">
+      {
+        listaTareas.map((e, index) => <Tarea 
+                                tarea={e}
+                                borrar={borrar}
+                                id={index}
+                                editar={actualizarTarea}
+                                />
+        )
+      }
+      </div>
+
     </div>
   );
 }
